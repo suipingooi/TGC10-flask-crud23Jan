16af7177449058602dabd7d089a5a7927f41d276
SuiPing <kam.suipingooi@me.com>
2021-01-23 03:22:41 +0000

    added customer route

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
 import os
 
 app = Flask(__name__)
+database = {}
+with open('customers.json') as fp:
+    database = json.load(fp)
+print(database)
+
 
 @app.route('/')
 def home():
     return render_template('home.template.html')
 
+
+@app.route('/customers')
+def show_customers():
+    return render_template('customers.template.html')
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
